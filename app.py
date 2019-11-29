4c07f8ac2b8e89138a3336f054a5b9b84743de7b
EscapistH <923737723@qq.com>
2019-11-30 00:15:52 +0800

    导入model类

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
 from flask import *
 
+# 导入controller
 from controller.index import index
 from controller.article import article
 from controller.photo import photo
 
+# 导入model
+from model.article_model import Article
+from model.photo_model import Photo
+
+# 导入配置
+from config import Config
 
 app = Flask(__name__)
-app.register_blueprint(photo, url_prefix='/photo')
-app.register_blueprint(article, url_prefix='/article')
+app.config.from_pyfile(Config)
 app.register_blueprint(index, url_prefix='/')
 app.register_blueprint(index, url_prefix='/index')
+app.register_blueprint(photo, url_prefix='/photo')
+app.register_blueprint(article, url_prefix='/article')
 
 
 @app.errorhandler(404)
