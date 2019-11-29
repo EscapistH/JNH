72aa7c043335e622ef0a87f5a57415a4d063fd3e
EscapistH <923737723@qq.com>
2019-11-30 00:15:32 +0800

    添加sqlalchemy配置项,添加secret_key

--- config.py
+++ config.py
@@ -1,2 +1,8 @@
 class Config(object):
+    # 设置数据库地址
     SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@127.0.0.1:3306/jnh'
+    # 追踪更新，设置False以压制waring
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # 设置 SECRET_KEY
+    SECRET_KEY = 'sk'
+
