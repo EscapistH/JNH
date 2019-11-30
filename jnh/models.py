3443c7d806f040d06dd0b0a293e09b4f485a802c
EscapistH <923737723@qq.com>
2019-11-30 19:01:35 +0800

    重写构造函数，在创建对象是传入参数

--- jnh/models.py
+++ jnh/models.py
@@ -10,8 +10,13 @@ class Article(db.Model):
     article_auth = db.Column(db.String(64))
     article_content = db.Column(db.Text)
 
+    def __init__(self, article_title, article_auth, article_content):
+        self.article_title = article_title
+        self.article_auth = article_auth
+        self.article_content = article_content
+
     def __repr__(self):
-        return '《{}》\n\t-- by {}\n{}'.format(self.article_title, self.article_auth, self.article_content)
+        return '《{}》\n\t-- by {}\n{}\n'.format(self.article_title, self.article_auth, self.article_content)
 
 
 class Photo(db.Model):
@@ -21,6 +26,10 @@ class Photo(db.Model):
     photo_id = db.Column(db.Integer, primary_key=True)
     photo_title = db.Column(db.String(64))
     photo_path = db.Column(db.String(64))
+
+    def __init__(self, photo_title, photo_path):
+        self.photo_title = photo_title
+        self.photo_path = photo_path
 
     def __repr__(self):
         return 'photo: {} at {}'.format(self.photo_title, self.photo_path)
