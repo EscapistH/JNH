10c715c6d9106461cdb847849ace05834572db8f
EscapistH <923737723@qq.com>
2019-12-01 09:22:38 +0800

    开启sqlalchemy自动提交功能

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
 class Config(object):
     # 设置数据库地址
     SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@127.0.0.1:3306/jnh'
-    # 追踪更新，设置False以压制waring
+    # 关闭数据追踪，设置False以压制waring
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # 开启自动提交
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     # 设置 SECRET_KEY
     SECRET_KEY = 'sk'
 
